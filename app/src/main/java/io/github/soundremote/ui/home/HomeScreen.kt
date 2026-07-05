ca585e49e698d5ab3cd0d2f954db966de058d4f2
唐林小宝 <lqq1119@sina.com>
2026-07-05 17:10:51 +0800

    style(ui): 调整HomeScreen中地址编辑的垂直内边距
    - 将地址编辑组件的垂直内边距从5dp减少到2dp
    - 保持水平方向的内边距16dp不变
    - 优化布局视觉间距，使界面更紧凑

--- app/src/main/java/io/github/soundremote/ui/home/HomeScreen.kt
+++ app/src/main/java/io/github/soundremote/ui/home/HomeScreen.kt
@@ -212,7 +212,7 @@ fun HomeScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight(unbounded = true)
-                    .padding(horizontal = 16.dp, vertical = 5.dp)
+                    .padding(horizontal = 16.dp, vertical = 2.dp)
             ) {
                 AddressEdit(
                     address = address,
